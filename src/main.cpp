ca21732b82972504bcf6c2e9d86c1d6e74fffb02
rockcutter <rockcutter414@gmail.com>
2021-11-01 04:30:36 +0900

    [add] Add Ready message for issue #3

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <memory>
 #include "main.h"
 #include "sleepy_discord/sleepy_discord.h"
+#include "util/ConsoleOut.h"
 
 std::shared_ptr<MyClientClass> client(new MyClientClass("ODQ1MTg4MTEyNDA0NDQ3MjQ3.YKdUxA.70fZv1c-yDcl0LDEKrQbJpfHzaI", SleepyDiscord::USER_CONTROLED_THREADS));
 
@@ -10,6 +11,7 @@ void MyClientClass::onMessage(SleepyDiscord::Message message) {
 }
 
 void MyClientClass::onReady(SleepyDiscord::Ready readyData) {
+	Util::ConsoleOut("Ready");
 	return;
 }
 
