c9255aaa5b58933bf4453bd1221e80c1f90a5a7e
rockcutter <rockcutter414@gmail.com>
2021-10-31 23:17:16 +0900

    [add] Add basic fuctions for issue #1

--- src/main.h
+++ src/main.h
@@ -1,2 +1,10 @@
 #pragma once
+#include "sleepy_discord/sleepy_discord.h"
 
+class MyClientClass : public SleepyDiscord::DiscordClient {
+
+public :
+	using SleepyDiscord::DiscordClient::DiscordClient;
+	void onReady(SleepyDiscord::Ready) override;
+	void onMessage(SleepyDiscord::Message message) override;
+};
