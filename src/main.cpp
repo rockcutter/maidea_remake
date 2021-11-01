2770b3ecd4dafb3b479aafb8b3bb0e92719b2896
rockcutter <rockcutter414@gmail.com>
2021-11-02 00:35:39 +0900

    [add] add process of loading discord token from config file

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,19 @@
 #include <memory>
 #include "main.h"
 #include "client/MyClient.h"
+#include "util/config/Config.h"
 
-std::shared_ptr<MyClientClass> client(new MyClientClass("ODQ1MTg4MTEyNDA0NDQ3MjQ3.YKdUxA.70fZv1c-yDcl0LDEKrQbJpfHzaI", SleepyDiscord::USER_CONTROLED_THREADS));
+constexpr char CONFIG_FILE_PATH[] = "maidea.cfg";
+constexpr char CONFIG_DISCORD_TOKEN_KEY[] = "DISCORD_TOKEN";
+
+std::string discordToken = Util::LoadConfig(CONFIG_FILE_PATH)->at(CONFIG_DISCORD_TOKEN_KEY);
+
+std::shared_ptr<MyClientClass> client(
+	new MyClientClass(
+		discordToken,
+		SleepyDiscord::USER_CONTROLED_THREADS
+	)
+);
 
 int main() {
 	client->run();	
