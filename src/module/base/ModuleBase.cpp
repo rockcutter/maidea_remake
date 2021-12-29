13cf13ba2ccbf44afa3faf2cfc06118c29e8fd79
rockcutter <rockcutter414@gmail.com>
2021-12-30 01:28:08 +0900

    [change] change type of option to pointer

--- src/module/base/ModuleBase.cpp
+++ src/module/base/ModuleBase.cpp
@@ -1,6 +1,6 @@
 #include "ModuleBase.h"
 
 namespace Module {
-	ModuleBase::ModuleBase(std::string moduleName, std::string command, boost::program_options::option_description od): moduleName(moduleName), command(command), options(od)
+	ModuleBase::ModuleBase(std::string moduleName, std::string command, boost::program_options::option_description* od): moduleName(moduleName), command(command), options(od)
 	{}
 }
--- src/module/hello/Hello.cpp
+++ src/module/hello/Hello.cpp
@@ -2,7 +2,7 @@
 #include <boost/format.hpp>
 
 namespace Module {
-	Hello::Hello(): ModuleBase(HELLO_MODULE_NAME, HELLO_COMMAND, boost::program_options::option_description()), iomodule(HELLO_MODULE_NAME){}
+	Hello::Hello(): ModuleBase(HELLO_MODULE_NAME, HELLO_COMMAND, nullptr), iomodule(HELLO_MODULE_NAME){}
 
 	void Hello::Send(SleepyDiscord::Snowflake<SleepyDiscord::Channel> channelID) {
 		this->iomodule.Send(channelID, "Hello!");
