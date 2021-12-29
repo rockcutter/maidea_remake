93f345b4d312e413c8f564c9a84a1f3b73cd2fdf
rockcutter <rockcutter414@gmail.com>
2021-12-30 00:46:34 +0900

    [add] add CommandHandler for issue #45

--- src/handler/CommandHandler.cpp
+++ src/handler/CommandHandler.cpp
@@ -1,8 +1,35 @@
+#include <unordered_map>
+#include <vector>
+#include <boost/algorithm/string.hpp>
 #include "CommandHandler.h"
+#include "module/hello/Hello.h"
+
 
 namespace Handler {
+	std::unordered_map<std::string, std::unique_ptr<Module::ModuleBase>> commandMap = std::move(FillCommandMap());
+
+	std::unordered_map<std::string, std::unique_ptr<Module::ModuleBase>> FillCommandMap() {
+		std::unordered_map<std::string, std::unique_ptr<Module::ModuleBase>> m;
+		std::vector<std::unique_ptr<Module::ModuleBase>> moduleArray;
+
+		//ここで追加したモジュールのインスタンスをemplace_back
+		moduleArray.emplace_back(std::unique_ptr<Module::ModuleBase>(new Module::Hello()));
+
+		//ここまで
+		for (auto& p : moduleArray) {
+			m[p->GetCommand()] = std::move(p);
+		}
+		return m;
+	}
+
 	void CommandHandler(SleepyDiscord::Message message) {
+		if (message.author.bot) {
+			return;
+		}
+		std::vector<std::string> tmp;
+		boost::algorithm::split(tmp, message.content, boost::is_space());
 		
+		commandMap.at(tmp.at(0).erase(0, 1))->Handler(message);
 		return;
 	}
 }
