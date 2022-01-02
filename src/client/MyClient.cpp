d61c572c79fadce5848c1ff89a1404370dac6a3b
rockcutter <rockcutter414@gmail.com>
2022-01-03 01:53:10 +0900

    [add] implement repeat message for issue #49

--- src/client/MyClient.cpp
+++ src/client/MyClient.cpp
@@ -1,5 +1,6 @@
 #include "MyClient.h"
 #include "handler/MessageHandler.h"
+#include "handler/ReactionHandler.h"
 #include "util/ConsoleOut.h"
 
 
@@ -10,6 +11,28 @@ void MyClientClass::onMessage(SleepyDiscord::Message message) {
 
 void MyClientClass::onReady(SleepyDiscord::Ready readyData) {
 	Util::ConsoleOut("Ready");
+	return;
+}
+
+void MyClientClass::onReaction(
+	SleepyDiscord::Snowflake<SleepyDiscord::User> userID, 
+	SleepyDiscord::Snowflake<SleepyDiscord::Channel> channelID, 
+	SleepyDiscord::Snowflake<SleepyDiscord::Message> messageID, 
+	SleepyDiscord::Emoji emoji) {
+	
+	SleepyDiscord::User user = getUser(userID);
+	if (user.bot) {
+		return;
+	}
+
+	//repeat
+	if (emoji.name == "🔁") {
+		SleepyDiscord::Message message;
+		addReaction(channelID, messageID, emoji.name);
+		getMessage(channelID, messageID).cast(message);
+		this->onMessage(message);
+	}
+
 	return;
 }
 
