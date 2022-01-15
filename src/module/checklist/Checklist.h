b3eb7692dd2b8bd55ae5ce37dc75daeb4431f4cd
rockcutter <rockcutter414@gmail.com>
2022-01-15 13:16:23 +0900

    [add] implement Checklist module

---
+++ src/module/checklist/Checklist.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "sleepy_discord/sleepy_discord.h"
+#include "module/base/ModuleBase.h"
+#include "module/io/IOModule.h"
+
+namespace Module {
+	class Checklist : public ModuleBase {
+	private:
+		IOModule iomodule;
+		static std::vector<std::string> channels;
+	public:
+		Checklist();
+		static bool isEnable(SleepyDiscord::Snowflake<SleepyDiscord::Channel> channelID);
+		void Handler(SleepyDiscord::Message message) override;
+		bool Enable(SleepyDiscord::Snowflake<SleepyDiscord::Channel> channelID);
+		bool Disable(SleepyDiscord::Snowflake<SleepyDiscord::Channel> channelID);
+	};
+}
