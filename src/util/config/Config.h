b06b4a95284aac2aebec5e4f344fab2bb7c528f1
rockcutter <rockcutter414@gmail.com>
2022-01-18 21:14:05 +0900

    [delete] delete user-difined function which loads config

--- src/util/config/Config.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <map>
-#include <memory>
-#include <string>
-#include <fstream>
-
-namespace Util {
-	std::unique_ptr<std::map<std::string, std::string>> LoadConfig(std::string filePath);
-}
