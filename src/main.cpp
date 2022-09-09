00f4ce1395acfbc009c09ab44747cb0deb2f38ee
rockcutter <rockcutter414@gmail.com>
2022-09-09 20:43:50 +0900

    [add] add ReadConfigFile function

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,38 @@
 #include "main.h"
 #include "client/MyClient.h"
 #include "module/possession/Possession.h"
+#include "util/ReadConfig.h"
 
 constexpr char CONFIG_FILE_PATH[] = "maidea.cfg";
 constexpr char CONFIG_DISCORD_TOKEN_KEY[] = "DISCORD_TOKEN";
 
 namespace program_options = boost::program_options;
 
+#define DEBUG
+
 int main() {
+	//config読み取り
+	std::unordered_map<std::string, std::string> configMap;
+	std::vector<std::string> configKeyArray = {
+		CONFIG_DISCORD_TOKEN_KEY,
+		"PRIVILEGED_USER",
+	};
+	Util::ReadConfigFIle(CONFIG_FILE_PATH, configMap, configKeyArray);
+
+	//PossesionModuleのための初期化
+	Module::Possession::RegisterPrivilegedUser(
+		configMap["PRIVILEGED_USER"]
+	);
+
+	//bot client起動
+	auto client = MyClientClass::InitInstance(configMap[CONFIG_DISCORD_TOKEN_KEY], SleepyDiscord::USER_CONTROLED_THREADS);
+	client->setIntents(SleepyDiscord::Intent::SERVER_MESSAGES, SleepyDiscord::Intent::SERVER_MESSAGE_REACTIONS);
+	client->run();
+
+	return 0;
+	
+
+#ifndef DEBUG
 	//コンフィグファイルからコンフィグを取得
 	program_options::options_description configDescription("configration");	
 	program_options::variables_map configvm;
@@ -31,4 +56,5 @@ int main() {
 	//実行
 	client->setIntents(SleepyDiscord::Intent::SERVER_MESSAGES, SleepyDiscord::Intent::SERVER_MESSAGE_REACTIONS);
 	client->run();
+#endif
 }
