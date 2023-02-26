0d5f2814c543971507bdeb19a26348ac5da3fd10
= <=>
2023-02-27 00:41:39 +0900

    [add] Added processing to set the file path of the config

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,18 @@
 #include <memory>
+#include <string_view>
 #include "util/Config.hpp"
 #include "client/MyClient.h"
 #include "Initialize.hpp"
 
-constexpr char CONFIG_FILE_PATH[] = "maidea.cfg";
-constexpr char CONFIG_DISCORD_TOKEN_KEY[] = "DISCORD_TOKEN";
+inline constexpr std::string_view CONFIG_FILE_PATH 			= "maidea.cfg";
+inline constexpr std::string_view CONFIG_DISCORD_TOKEN_KEY 	= "DISCORD_TOKEN";
 
 int main(int argc, const char * const * const argv) {
 	InitializeApp();
+	Config::SetConfigFilePath(CONFIG_FILE_PATH);
 	auto&& configMap = Config::GetConfig();
 
-	auto&& client = MyClientClass::InitInstance(configMap[CONFIG_DISCORD_TOKEN_KEY], SleepyDiscord::USER_CONTROLED_THREADS);
+	auto&& client = MyClientClass::InitInstance(configMap[std::string{CONFIG_DISCORD_TOKEN_KEY}], SleepyDiscord::USER_CONTROLED_THREADS);
 	client->setIntents(SleepyDiscord::Intent::SERVER_MESSAGES, SleepyDiscord::Intent::SERVER_MESSAGE_REACTIONS);
 	client->run();
 
