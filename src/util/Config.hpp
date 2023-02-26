883f64daa8d3cedd6170a990302fbc8b333df368
= <=>
2023-02-26 21:27:38 +0900

    [add] Add class to manage config

--- src/util/Config.hpp
+++ src/util/Config.hpp
@@ -3,18 +3,37 @@
 #include <string>
 #include <string_view>
 #include <fstream>
-
+#include "SplitString.hpp"
 
 class Config {
 private:
 	static inline bool isConfigLoaded = false;
 	static inline std::unordered_map<std::string, std::string> configMap{};
+	static inline std::string_view configFilePath = "maidea.cfg";
 	
 	static void LoadConfig() {
+		configMap.clear();
 
+		std::ifstream ifs{std::string{Config::configFilePath}};
+		if(!ifs) {
+			return;
+		}
+
+		std::string readLineStr;
+		std::vector<std::string> splitedStrArray;
+		while(getline(ifs, readLineStr)){
+			SplitString(splitedStrArray, readLineStr, "=");
+			if(splitedStrArray.size() != 2) continue;
+			Config::configMap.emplace(splitedStrArray[0], splitedStrArray[1]);
+		}
 	}
 
 public:
+
+	static void SetConfigFilePath(std::string_view filePath){
+		Config::configFilePath = filePath;
+	}
+
 	static void ReloadConfig(){
 		Config::LoadConfig();
 	}
