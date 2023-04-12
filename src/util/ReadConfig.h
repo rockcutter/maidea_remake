51806cbe0903eb1b30dd308a79b662367c10365e
rockcutter <rockcutter414@gmail.com>
2023-04-12 19:09:02 +0900

    [remove] ReadConfig.cpp/hppを削除 #133

--- src/util/ReadConfig.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include <unordered_map>
-#include <vector>
-
-namespace Util {
-	void ReadConfigFIle(const std::string& configFilePath, std::unordered_map<std::string, std::string>& configMap, const std::vector<std::string> configKeyArray);
-}
